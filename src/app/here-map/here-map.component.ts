1f8ffcbade5492a2e7deeb3e96cf25b6ed0e942b
garry <garry@synthesis.co.za>
2019-10-13 10:14:59 +0200

    Reverted most changes

--- src/app/here-map/here-map.component.ts
+++ src/app/here-map/here-map.component.ts
@@ -66,13 +66,13 @@ export class HereMapComponent implements OnInit {
             center: { lat: this.lat, lng: this.lng }
         }
     );
-        console.log("PLATFORM:=> ", this.map);
-    this.routee = this.hereRoute(this.map, this.platform, {
-        'mode': 'fastest;car',
-        'representation': 'display',
-        'waypoint0': 'geo!-26.145314,27.936605',
-        'waypoint1': 'geo!-26.142515,28.046407'
-    })
+    //     console.log("PLATFORM:=> ", this.map);
+    // this.routee = this.hereRoute(this.map, this.platform, {
+    //     'mode': 'fastest;car',
+    //     'representation': 'display',
+    //     'waypoint0': 'geo!-26.145314,27.936605',
+    //     'waypoint1': 'geo!-26.142515,28.046407'
+    // })
 
     console.log("ROUTEEEE!=> ", this.routee);
 
@@ -121,13 +121,95 @@ export class HereMapComponent implements OnInit {
     //   }
     // );
 
+    var routingParameters = {
+        // The routing mode:
+        'mode': 'fastest;car',
+        // The start point of the route:
+        'waypoint0': 'geo!-26.125314,27.836605',
+        // The end point of the route:
+        'waypoint1': 'geo!-26.162515,28.076407',
+        // To retrieve the shape of the route we choose the route
+        // representation mode 'display'
+        'representation': 'display'
+      };
 
-    const mapEvents = new H.mapevents.MapEvents(this.map);
-    let behavior = new H.mapevents.Behavior(mapEvents);
+    console.log("Routing Params=> ", routingParameters);
+
+    // =======================================================
+
+
+    // var onResult = function(result) {
+    //     var route,
+    //     routeShape,
+    //     startPoint,
+    //     endPoint,
+    //     linestring;
+    //     if(result.response.route) {
+    //     // Pick the first route from the response:
+    //     route = result.response.route[0];
+    //     // Pick the route's shape:
+    //     routeShape = route.shape;
+      
+    //     // Create a linestring to use as a point source for the route line
+    //     linestring = new H.geo.LineString();
+      
+    //     // Push all the points in the shape into the linestring:
+    //     routeShape.forEach(function(point) {
+    //     var parts = point.split(',');
+    //     linestring.pushLatLngAlt(parts[0], parts[1]);
+    //     });
+      
+    //     // Retrieve the mapped positions of the requested waypoints:
+    //     startPoint = route.waypoint[0].mappedPosition;
+    //     endPoint = route.waypoint[1].mappedPosition;
+      
+    //     // Create a polyline to display the route:
+    //     var routeLine = new H.map.Polyline(linestring, {
+    //     style: { strokeColor: 'blue', lineWidth: 5 }
+    //     });
+      
+    //     // Create a marker for the start point:
+    //     var startMarker = new H.map.Marker({
+    //     lat: startPoint.latitude,
+    //     lng: startPoint.longitude
+    //     });
+      
+    //     // Create a marker for the end point:
+    //     var endMarker = new H.map.Marker({
+    //     lat: endPoint.latitude,
+    //     lng: endPoint.longitude
+    //     });
+      
+    //     // Add the route polyline and the two markers to the map:
+    //     this.map.addObjects([routeLine, startMarker, endMarker]);
+      
+    //     // Set the map's viewport to make the whole route visible:
+    //     this.map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
+    //     }
+    //   };
+
+    // Get an instance of the routing service:
+    // var router = this.platform.getRoutingService();
+    // let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    // Call calculateRoute() with the routing parameters,
+    // the callback and an error callback function (called if a
+    // communication error occurs):
+    // router.calculateRoute(routingParameters, onResult,
+    // function(error) {
+    // alert(error.message);
+    // });
+
+
+    // =======================================================
+
+
+
+    // const mapEvents = new H.mapevents.MapEvents(this.map);
+    // let behavior = new H.mapevents.Behavior(mapEvents);
     const marker1 = new H.map.Marker({lat: -26.145314, lng:27.936605});
     const marker2 = new H.map.Marker({lat: -26.1425, lng:28.046407});
-    // const marker3 = new H.map.Marker({lat: -26.1, lng:28.0});
-    // const marker4 = new H.map.Marker({lat: -26.142515, lng:28.046407});
+    const marker3 = new H.map.Marker({lat: -26.1, lng:28.0});
+    const marker4 = new H.map.Marker({lat: -26.142515, lng:28.046407});
 
     // const marker3 = new H.map.Marker({lat: -26.145314, lng:27.936605});
     // const marker4 = new H.map.Marker({lat: 26.1425, lng:28.0464});
@@ -136,26 +218,29 @@ export class HereMapComponent implements OnInit {
     const lineString = new H.geo.LineString();
     lineString.pushPoint(marker1.getPosition());
     lineString.pushPoint(marker2.getPosition());
-    // lineString.pushPoint(marker3.getPosition());
-    // lineString.pushPoint(marker4.getPosition());
+    lineString.pushPoint(marker3.getPosition());
+    lineString.pushPoint(marker4.getPosition());
     const polyline = new H.map.Polyline(
         lineString,
         {
             style: {
-                strokeColor: "skyblue",
+                strokeColor: "yellow",
                 lineWidth: 3
             }
         }
     );
     
     const circle = new H.map.Circle(marker1.getPosition(), 100)
-    this.map.addObjects([marker1, marker2, polyline, circle]);
+    this.map.addObjects([marker1, marker2, marker3, marker4, polyline, circle]);
     const distance = marker1.getPosition().distance(marker2.getPosition());
-    // console.log("Distance between points: ", distance);
+    console.log("Distance between points: ", distance);
 
-    // let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
     this.ui = H.ui.UI.createDefault(this.map, defaultLayers);
 }
+
+
+
 
 public places(query: string) {
   this.map.removeObjects(this.map.getObjects());
